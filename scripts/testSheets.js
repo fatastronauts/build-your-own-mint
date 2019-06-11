9608749d8a23c85cdfd785f3067a67a5c9b2eb91
Benjamin Cooper <bencooper222@gmail.com>
2019-06-11 00:26:19 -0700

    fix test-sheets

--- scripts/testSheets.js
+++ scripts/testSheets.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
-const { updateSheet } = require('../lib/update');
+const { updateTransactions } = require('../lib/updaters/gsheets');
 
-updateSheet([
+// sloppy but it works
+// I wouldn't necessarily call this a supported use of that API.
+// TODO: come up with a better way to test the sheets functionality (...separate package?)
+updateTransactions([
   {
     range: 'A1',
     value: 'It worked!',
