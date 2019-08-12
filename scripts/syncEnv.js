45015062d88daf70e1c90d64a7947f7b4ddd0ca2
Benjamin Cooper <bencooper222@gmail.com>
2019-08-12 15:38:37 -0500

    Sync env var script written

---
+++ scripts/syncEnv.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+
+dotenv.config();
+const { GITHUB_USERNAME, GITHUB_PROJECT, CIRCLE_CI_TOKEN } = process.env;
+
+// variables that shouldn't be synced
+const FILTERED_VARIABLES = [
+  'CIRCLE_CI_TOKEN',
+  'ENABLE_SMS', // sms is typically different for debugging and otherwise
+  'GITHUB_USERNAME',
+  'GITHUB_PROJECT',
+];
+
+const updateSingleToken = async (name, value) => {
+  console.log(
+    'Updated this pair: ' +
+      (await fetch(
+        `https://circleci.com/api/v1.1/project/github/${GITHUB_USERNAME}/${GITHUB_PROJECT}/envvar?circle-token=${CIRCLE_CI_TOKEN}`,
+        {
+          method: 'post',
+          body: JSON.stringify({ name, value }),
+          headers: { 'Content-Type': 'application/json' },
+        },
+      ).then(res => res.json())),
+  );
+};
+
+const syncAllVariables = async () => {
+  const envPath = path.resolve(__dirname, '../.env');
+  const envVars = dotenv.parse(fs.readFileSync(envPath));
+
+  // converts to an array of name-value pairs that we can send to circleci
+  const filteredPairs = Object.entries(
+    Object.keys(envVars)
+      .filter(key => !FILTERED_VARIABLES.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = envVars[key];
+        return obj;
+      }, {}),
+  ).map(arr => {
+    return { name: arr[0], value: arr[1] };
+  });
+
+  for (let i = 0; i < filteredPairs.length; i++) {
+    await updateSingleToken(filteredPairs[i].name, filteredPairs[i].value);
+  }
+};
+
+syncAllVariables();
