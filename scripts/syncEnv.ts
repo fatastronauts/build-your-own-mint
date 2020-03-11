b7e0f3d66787cc4973da1d9ecc38944ff2273d84
Benjamin Cooper <bencooper222@gmail.com>
2020-03-11 21:10:20 +0000

    change variable syncing script to typescript

--- scripts/syncEnv.ts
+++ scripts/syncEnv.ts
@@ -1,18 +1,26 @@
-const dotenv = require('dotenv');
-const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
+import { config as dotenvConfig, parse as dotenvParse } from 'dotenv';
+import { readFileSync } from 'fs';
+import { resolve as pathResolve } from 'path';
+import fetch from 'node-fetch';
 
-dotenv.config();
+dotenvConfig();
 const { GITHUB_USERNAME, GITHUB_PROJECT, CIRCLE_CI_TOKEN } = process.env;
+if (!GITHUB_USERNAME) throw new Error('No GITHUB_USERNAME available!');
+if (!GITHUB_PROJECT) throw new Error('No GITHUB_PROJECT available!');
+if (!CIRCLE_CI_TOKEN) throw new Error('No CIRCLE_CI_TOKEN available!');
 
 // variables that shouldn't be synced
-const FILTERED_VARIABLES = [
+const filteredVariables = [
   'CIRCLE_CI_TOKEN',
   'ENABLE_SMS', // sms is typically different for debugging and otherwise
   'GITHUB_USERNAME',
   'GITHUB_PROJECT',
 ];
+
+interface NameValPair {
+  name: string;
+  value: string;
+}
 
 const deleteCurrentEnvironmentVariables = async () => {
   const envVars = await (
@@ -37,7 +45,7 @@ const deleteCurrentEnvironmentVariables = async () => {
   console.log('Finished deleting env vars');
 };
 
-const updateSingleToken = async nameValToken => {
+const updateSingleToken = async (nameValToken: NameValPair) => {
   console.log(
     'Updated this pair: ' +
       JSON.stringify(
@@ -56,23 +64,19 @@ const updateSingleToken = async nameValToken => {
 const syncAllVariables = async () => {
   await deleteCurrentEnvironmentVariables(); // clear out current env vars
 
-  const envPath = path.resolve(__dirname, '../.env');
-  const envVars = dotenv.parse(fs.readFileSync(envPath));
+  const envPath = pathResolve(__dirname, '../.env');
+  const envVars = dotenvParse(readFileSync(envPath));
+
+  // don't sync certain variables
+  for (let key in envVars) if (filteredVariables.includes(key)) delete envVars[key];
 
   // converts to an array of name-value pairs that we can send to circleci
-  const filteredPairs = Object.entries(
-    Object.keys(envVars)
-      .filter(key => !FILTERED_VARIABLES.includes(key))
-      .reduce((obj, key) => {
-        obj[key] = envVars[key];
-        return obj;
-      }, {}),
-  ).map(([name, value]) => {
-    return { name, value };
-  });
+  const arrayOfNameValPairs: NameValPair[] = Object.entries(
+    envVars,
+  ).map(([name, value]) => ({ name, value }));
 
-  for (let i = 0; i < filteredPairs.length; i++) {
-    await updateSingleToken(filteredPairs[i]);
+  for (let pair of arrayOfNameValPairs) {
+    await updateSingleToken(pair);
   }
 };
 
